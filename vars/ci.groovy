5dd4cb9136b8204a554e57796e3d58507eec81bf
nfuenzalidam <69167199+nfuenzalidam@users.noreply.github.com>
2022-02-05 13:19:19 -0300

    Update ci.groovy
    fix 9

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -51,14 +51,7 @@ def sSonar(){
     env.STAGE = "Stage Sonar"
     stage("$env.STAGE"){
         withSonarQubeEnv('sonarqube') {
-            sh "echo $env.GIT_REPO_NAME"
-            sh "echo $env.GIT_BRANCH"
-            sh "echo $env.BUILD_NUMBER"
-            def cappedBranch = "$env.GIT_BRANCH"
-            env.BRANCH_SONAR = cappedBranch.split('/')[1]
-            //sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=${env.GIT_REPO_NAME}-${env.GIT_BRANCH}-${env.BUILD_NUMBER} -Dsonar.java.binaries=build'
-            sh 'echo "-Dsonar.projectKey=$env.GIT_REPO_NAME-$env.BRANCH_SONAR-$env.BUILD_NUMBER -Dsonar.java.binaries=build"'
-            sh "mvn clean verify sonar:sonar -Dsonar.projectKey=$env.GIT_REPO_NAME-$env.BRANCH_SONAR-$env.BUILD_NUMBER -Dsonar.java.binaries=build"
+            sh "mvn clean verify sonar:sonar -Dsonar.projectKey=$env.GIT_REPO_NAME-$env.BRANCH-$env.BUILD_NUMBER -Dsonar.java.binaries=build"
         }
     }
 }
