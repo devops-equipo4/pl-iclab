b622533a508178623656f2c8eeac05fefe3f4da2
luismella <luismellareyes@gmail.com>
2022-02-05 23:15:45 -0300

    modificacion ci

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,15 +1,15 @@
 import utilities.*
 
 //def call(stages){
-def call(){
+def call() {
 
     def listStagesOrder = [
-        'compile': 'sCompile',
-        'unitTest': 'sUnitTest',
-        'jar': 'sJar',
-        'sonar': 'sSonar',
-        'nexusUpload': 'sNexusUpload',
-        'gitCreateRelease': 'sGitCreateRelease',
+            'compile'         : 'sCompile',
+            'unitTest'        : 'sUnitTest',
+            'jar'             : 'sJar',
+            'sonar'           : 'sSonar',
+            'nexusUpload'     : 'sNexusUpload',
+            'gitCreateRelease': 'sGitCreateRelease',
     ]
     allStages()
     //if (stages.isEmpty()){
@@ -17,7 +17,7 @@ def call(){
     //}
 }
 
-def allStages(){
+def allStages() {
     sCompile()
     sUnitTest()
     sJar()
@@ -26,64 +26,68 @@ def allStages(){
     sGitCreateRelease()
 }
 
-def sCompile(){
+def sCompile() {
     env.STAGE = "Stage Compile"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         sh "mvn clean compile -e"
     }
 }
 
-def sUnitTest(){
+def sUnitTest() {
     env.STAGE = "Stage Unit Test"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         sh "mvn clean test -e"
     }
 }
 
-def sJar(){
+def sJar() {
     env.STAGE = "Stage Jar"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         sh "mvn clean package -e"
     }
 }
 
-def sSonar(){
+def sSonar() {
     env.STAGE = "Stage Sonar"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         withSonarQubeEnv('sonarqube') {
             sh "mvn clean verify sonar:sonar -Dsonar.projectKey=$env.GIT_REPO_NAME-$env.BRANCH-$env.BUILD_NUMBER -Dsonar.java.binaries=build"
         }
     }
 }
 
-def sNexusUpload(){
+def sNexusUpload() {
     env.STAGE = "Stage Nexus Upload"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         nexusPublisher nexusInstanceId: 'nexus',
-        nexusRepositoryId: 'devops-usach-nexus',
-        packages: [
-            [$class: 'MavenPackage',
-                mavenAssetList: [
-                    [classifier: '',
-                    extension: '.jar',
-                    filePath: 'build/DevOpsUsach2020-0.0.1.jar'
+                nexusRepositoryId: 'devops-usach-nexus',
+                packages: [
+                        [$class         : 'MavenPackage',
+                         mavenAssetList : [
+                                 [classifier: '',
+                                  extension : '.jar',
+                                  filePath  : 'build/DevOpsUsach2020-0.0.1.jar'
+                                 ]
+                         ],
+                         mavenCoordinate: [
+                                 artifactId: 'DevOpsUsach2020',
+                                 groupId   : 'com.devopsusach2020',
+                                 packaging : 'jar',
+                                 version   : '0.0.1'
+                         ]
+                        ]
                 ]
-            ],
-                mavenCoordinate: [
-                    artifactId: 'DevOpsUsach2020',
-                    groupId: 'com.devopsusach2020',
-                    packaging: 'jar',
-                    version: '0.0.1'
-                ]
-            ]
-        ]
     }
 }
 
-def sGitCreateRelease(){
+def sGitCreateRelease() {
     env.STAGE = "Stage Git Create Release"
-    stage("Stage Git Create Release"){
-        if(validations.validateBranchDevelop()){
+    stage("Stage Git Create Release") {
+
+        env.RETORNO = validations.validateBranchDevelop()
+
+        sh 'echo "RETORNO $env.RETORNO'
+        if (r) {
             sh 'echo "Release se realizará manual"'
         }
     }
