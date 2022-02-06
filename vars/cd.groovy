d44dc68936fe91525591d13b09036869ac7eff3f
luismella <luismellareyes@gmail.com>
2022-02-06 19:58:32 -0300

    modificacion cd

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -51,7 +51,7 @@ def sRun(){
 def sTest(){
     env.STAGE = "Stage Test"
     stage("$env.STAGE"){
-        
+        sh "sleep 60 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
     }
 }
 
