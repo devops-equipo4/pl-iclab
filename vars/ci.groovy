7ad1adc5af26add6788c4c23ef151f0bf6223588
luismella <luismellareyes@gmail.com>
2022-02-06 10:54:25 -0300

    modificacion ci

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -89,11 +89,11 @@ def sGitCreateRelease() {
             // sh 'git remote set-url origin git@github.com:devops-equipo4/ms-iclab.git'
             //  sh 'git config --global user.email "nestor.fuenzalida@gmail.com"'
             //sh 'git config --global user.name "nfuenzalidam"'
-
-            sh "git checkout develop && git pull origin develop"
-            sh "git checkout -b release-v$env.BUILD_NUMBER.0.0"
-            sh 'git add .'
-            sh 'git commit -am "creacion de release"'
+            sh 'echo "hola"'
+            //sh "git checkout develop && git pull origin develop"
+            // sh "git checkout -b release-v$env.BUILD_NUMBER.0.0"
+            //sh 'git add .'
+            // sh 'git commit -am "creacion de release"'
             //sh "echo 'git push --set-upstream origin release-v$env.BUILD_NUMBER.0.0'"
 
 
