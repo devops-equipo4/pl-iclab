7f1f761e9411afd8eec5318f3fe5688f81e3e6bb
luismella <luismellareyes@gmail.com>
2022-02-05 23:46:18 -0300

    modificacion ci

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -86,9 +86,8 @@ def sGitCreateRelease() {
     stage("Stage Git Create Release") {
         sh 'echo "RETORNO $env.STAGE"'
         if (env.GIT_BRANCH =~ "develop*") {
-            sh 'git flow init -d'
-            sh 'git flow release start v1.0.0'
-            sh 'git flow release publish v1.0.0'
+            sh 'git checkout -b release-v2.0.0'
+            sh 'git push origin release-v2.0.0'
         }
     }
 }
