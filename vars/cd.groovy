4a8b5b0495b611235dfb2cbd2b18ac43fcc7f94b
luismella <luismellareyes@gmail.com>
2022-02-06 19:54:00 -0300

    modificacion cd

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -37,7 +37,7 @@ def sGitDiff(){
 def sNexusDownload(){
     env.STAGE = "Stage Nexus Download"
     stage("$env.STAGE"){
-        sh 'curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8082//repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+        sh 'curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
     }
 }
 
