93365512632a7eb10851b8fea0b1dbbf27bcc127
luismella <luismellareyes@gmail.com>
2022-02-06 21:17:30 -0300

    modificacion cd

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -82,8 +82,8 @@ def sGitTagMain() {
     env.STAGE = "Stage Git Tag Main"
     stage("$env.STAGE") {
         sh "git checkout main"
-        sh "git tag -s v0.1 -m 'Release v0.1'"
-        sh "git push --tags"
+        sh "git tag -f v0.1 -m 'Release v0.1'"
+        sh "git push --tag"
     }
 }
 
