da53cb07ec97f0594c07b7c0170120d85f1bfb19
rmateluna <rodrigo_mateluna@msn.com>
2022-02-07 19:13:45 -0300

    update versionSplit

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -69,6 +69,9 @@ def sNexusUpload() {
     printf $env.GROUP
 
     stage("$env.STAGE") {
+
+        printf('build/'+$env.ARTIFACT+'-'+$env.VERSION+'.jar')
+
         nexusPublisher nexusInstanceId: 'nexus',
         nexusRepositoryId: 'devops-usach-nexus',
         packages: [
