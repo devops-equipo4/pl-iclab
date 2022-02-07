4e39a6fa859002904a76eae8ddf241114e5ec468
luismella <luismellareyes@gmail.com>
2022-02-07 18:09:10 -0300

    cd

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -77,7 +77,7 @@ def sGitMergeDevelop() {
         sh "git pull"
         sh "git merge ${env.BRANCH} --no-ff --allow-unrelated-histories"
         sh "git add ."
-        sh "git commit -m 'omitir! release v$env.BUILD_NUMBER.0.0'"
+        //sh "git commit -m 'omitir! release v$env.BUILD_NUMBER.0.0'"
         sh "git push origin develop"
     }
 }
