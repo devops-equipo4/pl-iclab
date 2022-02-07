67b033d3e8974be54668255f69226355c8f01936
luismella <luismellareyes@gmail.com>
2022-02-07 19:23:03 -0300

    pipeline

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -53,7 +53,7 @@ def sTest() {
     stage("$env.STAGE") {
         sh 'echo "test"'
 
-        sh "sleep 10"
+        sh "sleep 60"
         sh"curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
         sh"curl -X GET 'http://localhost:8082/rest/mscovid/estadoMundial'"
         sh"curl -X GET 'http://localhost:8082/rest/mscovid/estadoPais?pais=chile'"
@@ -79,7 +79,7 @@ def sGitMergeDevelop() {
         sh "git pull"
         sh "git merge ${env.BRANCH} --no-ff --allow-unrelated-histories"
         sh "git add ."
-        //sh "git commit -m 'omitir! release v$env.BUILD_NUMBER.0.0'"
+        sh "git commit -m 'omitir! release v$env.BUILD_NUMBER.0.0'"
         sh "git push origin develop"
     }
 }
