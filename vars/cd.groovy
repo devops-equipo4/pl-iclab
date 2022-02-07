949850d30a2ea650c60a322d401fab692b636c27
luismella <luismellareyes@gmail.com>
2022-02-06 21:01:48 -0300

    modificacion cd

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -1,15 +1,15 @@
 import utilities.*
 
-def call(){
+def call() {
 
     def listStagesOrder = [
-        'gitDiff': 'sGitDiff',
-        'nexusDownload': 'sNexusDownload',
-        'run': 'sRun',
-        'test': 'sTest',
-        'gitMergeMaster': 'sGitMergeMaster',
-        'gitMergeDevelop': 'sGitMergeDevelop',
-        'gitTagMaster': 'sGitTagMaster',
+            'gitDiff'        : 'sGitDiff',
+            'nexusDownload'  : 'sNexusDownload',
+            'run'            : 'sRun',
+            'test'           : 'sTest',
+            'gitMergeMaster' : 'sGitMergeMaster',
+            'gitMergeDevelop': 'sGitMergeDevelop',
+            'gitTagMaster'   : 'sGitTagMaster',
     ]
 
     //if (stages.isEmpty()){
@@ -17,52 +17,51 @@ def call(){
     //}
 }
 
-def allStages(){
+def allStages() {
     sGitDiff()
     sNexusDownload()
     sRun()
     sTest()
     sGitMergeMain()
-    //sGitMergeDevelop()
+    sGitMergeDevelop()
     //sGitTagMain()
 }
 
-def sGitDiff(){
+def sGitDiff() {
     env.STAGE = "Stage Git Diff"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         sh "git diff origin/main"
     }
 }
 
-def sNexusDownload(){
+def sNexusDownload() {
     env.STAGE = "Stage Nexus Download"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         sh 'curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
     }
 }
 
-def sRun(){
+def sRun() {
     env.STAGE = "Stage Run"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         sh 'nohup java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
     }
 }
 
-def sTest(){
+def sTest() {
     env.STAGE = "Stage Test"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         sh 'echo "test"'
-       // sh "sleep 60 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
+        // sh "sleep 60 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
     }
 }
 
-def sGitMergeMain(){
+def sGitMergeMain() {
     env.STAGE = "Stage Git Merge Main"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         sh "git checkout ${env.BRANCH}"
         sh "git pull"
         sh "git checkout main"
-        //sh "git pull"
         sh "git merge ${env.BRANCH} --no-ff --allow-unrelated-histories"
         sh "git push origin main"
     }
@@ -70,15 +69,16 @@ def sGitMergeMain(){
 
 def sGitMergeDevelop() {
     env.STAGE = "Stage Git Merge Develop"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         sh "git checkout develop"
-        sh "git merge ${env.BRANCH}"
+        sh "git merge ${env.BRANCH} --no-ff --allow-unrelated-histories"
+        sh "git push origin develop -m 'omitir! release'"
     }
 }
 
-def sGitTagMain(){
+def sGitTagMain() {
     env.STAGE = "Stage Git Tag Main"
-    stage("$env.STAGE"){
+    stage("$env.STAGE") {
         sh "git checkout main"
         sh "git tag ${env.BRANCH}"
     }
